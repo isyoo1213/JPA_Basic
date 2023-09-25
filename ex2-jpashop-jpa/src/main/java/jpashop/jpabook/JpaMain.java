05edcd06cb0230aa13d35b4e7019b26872618e8a
isyoo1213 <moethns@gmail.com>
2023-09-25 17:32:05 +0900

    feat: Add One-Way Relationship

--- ex2-jpashop-jpa/src/main/java/jpashop/jpabook/JpaMain.java
+++ ex2-jpashop-jpa/src/main/java/jpashop/jpabook/JpaMain.java
@@ -35,8 +35,11 @@ public class JpaMain {
             memberA.setUsername("memberA1");
 
             // *** 이 memberA를 Team에 소속시키고 싶은 경우
-            // -> FK 식별자를 직접 다루게 되어버림
-            memberA.setTeamId(teamA.getId());
+            //case1. FK 식별자를 직접 다루는 경우
+            //memberA.setTeamId(teamA.getId());
+
+            //case2. 엔터티/테이블 연관관계로 매핑이 완료된 경우
+            memberA.setTeam(teamA);
 
             em.persist(memberA);
 
@@ -44,18 +47,51 @@ public class JpaMain {
             // 지금 중요한 것은 아니니 신경쓰지 않아도 됨
             // -> 각각의 엔터티 테이블마다 id관리를 하고 싶다면 따로 식별자 매핑을 해주면 됨
 
+            /**
+             * find()를 비롯한 1차캐시를 참고하는 경우, hibernate의 DB 쿼리를 보고 싶다면?
+             * -> flush() + clear()
+             * -> flush를 통해 영속성 컨텍스트의 1차캐시 + 저장소의 쿼리를 DB로 날리고
+             * -> clear를 통해 영속성 컨텍스트 초기화
+             */
+
+            em.flush();
+            em.clear();
+
 
             //2. 조회
             MemberA findMemberA1 = em.find(MemberA.class, memberA.getId());
 
+            //case1. FK 식별자를 직접 다루는 경우
             //조회한 Member의 Team을 알기 위해서는 DB와 통신하며 끄집어내야 함
-            Long findTeamId = findMemberA1.getTeamId();
-            Team findTeam = em.find(Team.class, findTeamId);
+            //Long findTeamId = findMemberA1.getTeamId();
+            //Team findTeam = em.find(Team.class, findTeamId);
             // -> 연관관계가 없음 + 객체지향스럽지 않음 + '데이터' 중심의 모델링
             /** Table vs 객체
              * 'Table'은 FK를 통해 JOIN을 사용하고 연관된 '테이블'을 찾는다
              * '객체'는 '참조'를 사용해 연관된 '객체'를 찾는다.
              */
+
+            //case2. 엔터티/테이블 연관관계 매핑이 완료된 경우
+            Team findTeam = findMemberA1.getTeam();
+            System.out.println("findTeam.getName() = " + findTeam.getName());
+
+            /* Hibernate:
+                select
+                    membera0_.MEMBERA_ID as MEMBERA_1_2_0_,
+                    membera0_.TEAM_ID as TEAM_ID3_2_0_,
+                    membera0_.USERNAME as USERNAME2_2_0_,
+                    team1_.TEAM_ID as TEAM_ID1_5_1_,
+                    team1_.name as name2_5_1_
+                from
+                    MemberA membera0_
+                left outer join
+                    Team team1_
+                        on membera0_.TEAM_ID=team1_.TEAM_ID
+                where
+                    membera0_.MEMBERA_ID=? */
+
+            // -> 다른 설정이 없다면 join을 통해 '한번에' 가져오지만, 이를 분리해서 가져오는 방식도 존재 - LazyLoading
+            //    ex) @ManyToOne(fetch = FetchType.LAZY) / default는 FetchType.EAGER
 
             tx.commit();
         } catch (Exception e) {
--- ex2-jpashop-jpa/src/main/java/jpashop/jpabook/sampledomain/MemberA.java
+++ ex2-jpashop-jpa/src/main/java/jpashop/jpabook/sampledomain/MemberA.java
@@ -13,8 +13,16 @@ public class MemberA {
     private String username;
 
     // * 연관관계를 레퍼런스로 가져가는 것이 아닌 FK로 가져가는 경우
-    @Column(name = "TEAM_ID")
-    private Long teamId;
+    //@Column(name = "TEAM_ID")
+    //private Long teamId;
+
+    // * 연관관계를 2가지 관점에서 가져감
+    // 1. Entity 관점 - '객체', 즉 레퍼런스를 사용
+    // 2. DB Table 관점 - Table의 join할 column을 'FK'로 연결
+    // 1 + 2 -> 객체와 FK를 연결함으로써 Entity/Table의 관점 모두 연관관계 매핑
+    @ManyToOne
+    @JoinColumn(name = "TEAM_ID")
+    private Team team;
 
     public Long getId() {
         return id;
@@ -32,11 +40,11 @@ public class MemberA {
         this.username = username;
     }
 
-    public Long getTeamId() {
-        return teamId;
+    public Team getTeam() {
+        return team;
     }
 
-    public void setTeamId(Long teamId) {
-        this.teamId = teamId;
+    public void setTeam(Team team) {
+        this.team = team;
     }
 }
